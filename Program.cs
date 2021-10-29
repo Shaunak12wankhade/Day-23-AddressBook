9d11b6bd944ffe794e3119192dfa10e65ac75ba9
unknown <shaunak12wankhade@ggmail.com>
2021-10-29 23:37:00 +0530

    [Shaunak] Added : Search person in city or state

--- Program.cs
+++ Program.cs
@@ -9,17 +9,19 @@ namespace Day23AddressBook
             //Console.WriteLine("Welcome to AddressBook");
 
             AddressBook.addAddressBook();
-            Console.WriteLine("\n");
-            AddressBook.ContactsDisplay();
-            Console.WriteLine("\n");
-            AddressBook.EditContact();
-            Console.WriteLine("\n");
-            AddressBook.ContactsDisplay();
-            Console.WriteLine("\n");
-            AddressBook.ContactsDisplay();
-            AddressBook.ContactsDisplay();
-            AddressBook.ContactsDisplay();
-            AddressBook.ContactsDisplay();
+            AddressBook.PersonSearch();
+
+            //Console.WriteLine("\n");
+            //AddressBook.ContactsDisplay();
+            //Console.WriteLine("\n");
+            //AddressBook.EditContact();
+            //Console.WriteLine("\n");
+            //AddressBook.ContactsDisplay();
+            //Console.WriteLine("\n");
+            //AddressBook.ContactsDisplay();
+            //AddressBook.ContactsDisplay();
+            //AddressBook.ContactsDisplay();
+            //AddressBook.ContactsDisplay();
         }
     }
 }
