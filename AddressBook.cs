6bdf44ef921685420348f16c818aba076cfdb120
unknown <shaunak12wankhade@ggmail.com>
2021-10-29 22:23:26 +0530

    [Shaunak] Added : Created a contact

---
+++ AddressBook.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day23AddressBook
+{
+    class AddressBook
+    {
+        Contacts person = new Contacts();
+    }
+}
